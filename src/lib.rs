84ad2656d877d3a9881f3719aae5fba7f24a3264
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-06 13:35:45 +0000

    add note for todo #17 impl<T> FromResidual<Result<!, syn::Error>> for DiagnosticResult<T>

--- src/lib.rs
+++ src/lib.rs
@@ -80,6 +80,7 @@ pub type DiagnosticStream = DiagnosticResult<proc_macro2::TokenStream>;
 /// impl TryFrom<LitInt> for Even {
 ///     type Error = DiagnosticResult<Even>;
 ///     fn try_from(num: LitInt) -> Result<Even, DiagnosticResult<Even>> {
+///         // TODO: #17 allow direct ? from syn::Error
 ///         let num: i32 = num.base10_parse().unwrap();
 ///         if num % 2 == 0 {
 ///             std::result::Result::Ok(Even(num))
