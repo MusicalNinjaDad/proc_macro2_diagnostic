cd223ed67d4115cde3f40216828bf8bfc943a1e5
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-06 17:48:52 +0000

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 //! errors and handle them easily:
 //!
 //! - Top level diagnostics must be either an `Error` or a `Warning`
-//! - (Only) `Help` (& `Note`s -> still to do) can be added to a diagnostic
+//! - (Only) `Help` & `Note`s can be added to a diagnostic
 //! - `Error`s always span the original call site - add a Help or Note to add information related
 //!   to other spans
 //! - `Warning`s will always finish with a `Note` detailing the original call site
