e38def6362a595ae9e72391486a7f0ee4a949985
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-07 07:53:20 +0000

    document stability and MSRV

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,51 @@
 //! - Multi-level nesting is not possible
 //! - We do not provide a implementation of the full [proc_macro::Diagnostic] API. Other crates
 //!   attempt to do this, if that is what you are after.
+//!
+//! ## Stability & MSRV
+//!
+//! Given that this crate exposes an experimental API from std it makes use of experimental features
+//! which require a nightly toolchain.
+//!
+//! > 🔬 **Experimental Features**
+//! >
+//! > This crate makes use of the following experimental features:
+//! >
+//! > - [`#![feature(assert_matches)]`](https://github.com/rust-lang/rust/issues/82775) (stable since 2026-02-12)
+//! > - [`#![feature(never_type)]`](https://github.com/rust-lang/rust/issues/35121)
+//! > - [`#![feature(proc_macro_diagnostic)]`](https://github.com/rust-lang/rust/issues/54140)
+//! > - [`#![feature(try_trait_v2)]`](https://github.com/rust-lang/rust/issues/84277)
+//! >
+//! > This list includes any unstable features used by direct & transitive dependencies (currently, none).
+//! >
+//! > The authors consider all of the above features to be reliable and already well advanced in the
+//! > stabilisation process.
+//!
+//! ### Stability guarantees
+//!
+//! We run automated tests **every month** to ensure no fundamental changes affect this crate and
+//! test every PR against the current nightly, as well as the current equivalent beta & stable.
+//! If you find an issue before we do, please
+//! [raise an issue on github](https://github.com/MusicalNinjaDad/proc_macro2_diagnostic/issues).
+//!
+//! ### MSRV
+//!
+//! For those of you working with a pinned nightly (etc.) this crate supports rust 1.85.1 onwards
+//! (released as stable on 20225-03-18). We use [autocfg](https://crates.io/crates/autocfg/) to
+//! seamlessly handle features which have been stabilised since then.
+//!
+//! ### Dependencies
+//!
+//! We deliberately keep the dependency list short and pay attention to any transitive dependencies
+//! we bring in.
+//!
+//! Current dependency tree:
+//!
+//! ```text
+//! proc_macro2_diagnostic <- This crate
+//! └── proc-macro2
+//!     └── unicode-ident
+//! ```
 
 use std::fmt::Debug;
 
