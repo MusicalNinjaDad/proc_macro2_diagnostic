9663703147d8f264847d8f0b946d3f14d2320407
MusicalNinjaDad <102677655+MusicalNinjaDad@users.noreply.github.com>
2026-04-07 10:20:04 +0200

    Stability checks & info (#27)
    including handling of newly stabilised assert_matches via autocfg

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(assert_matches)]
+#![cfg_attr(not(stable_assert_matches), feature(assert_matches))]
 #![feature(never_type)]
 #![feature(proc_macro_diagnostic)]
 #![feature(try_trait_v2)]
@@ -20,6 +20,54 @@
 //! - Multi-level nesting is not possible
 //! - We do not provide a implementation of the full [proc_macro::Diagnostic] API. Other crates
 //!   attempt to do this, if that is what you are after.
+//!
+//! ## Stability & MSRV
+//!
+//! Given that this crate exposes an experimental API from std it makes use of experimental features
+//! which require a nightly toolchain.
+//!
+//! > 🔬 **Experimental Features**
+//! >
+//! > This crate makes use of the following experimental features:
+//! >
+//! > - [`#![feature(assert_matches)]`](https://github.com/rust-lang/rust/issues/82775) (stable since 2026-02-12)
+//! > - [`#![feature(never_type)]`](https://github.com/rust-lang/rust/issues/35121)
+//! > - [`#![feature(proc_macro_diagnostic)]`](https://github.com/rust-lang/rust/issues/54140)
+//! > - [`#![feature(try_trait_v2)]`](https://github.com/rust-lang/rust/issues/84277)
+//! >
+//! > This list includes any unstable features used by direct & transitive dependencies (currently, none).
+//! >
+//! > The authors consider all of the above features to be reliable and already well advanced in the
+//! > stabilisation process.
+//!
+//! You do not need to enable these in your own code, the list is for information only.
+//!
+//! ### Stability guarantees
+//!
+//! We run automated tests **every month** to ensure no fundamental changes affect this crate and
+//! test every PR against the current nightly, as well as the current equivalent beta & stable.
+//! If you find an issue before we do, please
+//! [raise an issue on github](https://github.com/MusicalNinjaDad/proc_macro2_diagnostic/issues).
+//!
+//! ### MSRV
+//!
+//! For those of you working with a pinned nightly (etc.) this crate supports every version of
+//! edition 2024 (rust 1.85.1 onwards, released as stable on 20225-03-18). We use
+//! [autocfg](https://crates.io/crates/autocfg/) to seamlessly handle features which have been
+//! stabilised since then.
+//!
+//! ### Dependencies
+//!
+//! We deliberately keep the dependency list short and pay attention to any transitive dependencies
+//! we bring in.
+//!
+//! Current dependency tree:
+//!
+//! ```text
+//! proc_macro2_diagnostic <- This crate
+//! └── proc-macro2
+//!     └── unicode-ident
+//! ```
 
 use std::fmt::Debug;
 
@@ -455,7 +503,11 @@ impl From<DiagnosticStream> for TokenStream1 {
 
 #[cfg(test)]
 mod tests {
+    #[cfg(not(stable_assert_matches))]
     use std::assert_matches::assert_matches;
+
+    #[cfg(stable_assert_matches)]
+    use std::assert_matches;
 
     use super::*;
 
