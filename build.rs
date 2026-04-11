25a61e98796b31d30c60f51720e71e25202fce98
MusicalNinjaDad <102677655+MusicalNinjaDad@users.noreply.github.com>
2026-04-11 19:54:57 +0200

    fix new location of unstable assert_matches (#40)

--- build.rs
+++ build.rs
@@ -1,6 +1,57 @@
-extern crate autocfg;
+use autocfg::AutoCfg;
 
 fn main() {
     let ac = autocfg::new();
-    ac.emit_path_cfg("std::assert_matches", "stable_assert_matches");
+    stable_feature(&ac, "assert_matches");
+    assert_matches_in_module(&ac);
+    assert_matches_in_root(&ac);
+}
+
+fn stable_feature(ac: &AutoCfg, feature: &'static str) {
+    let cfg = format!("stable_{feature}");
+    let code = format!(
+        r#"
+    #![deny(stable_features)]
+    #![feature({feature})]
+    "#
+    );
+
+    autocfg::emit_possibility(&cfg);
+    if ac.probe_raw(&code).is_err() {
+        autocfg::emit(&cfg);
+    }
+}
+
+fn assert_matches_in_root(ac: &AutoCfg) {
+    let cfg = "assert_matches_in_root";
+    let code = r#"
+    #![allow(stable_features)]
+    #![feature(assert_matches)]
+    use std::assert_matches;
+
+    fn main() {
+        assert_matches!(Some(4), Some(_));
+    }
+        "#;
+    autocfg::emit_possibility(cfg);
+    if ac.probe_raw(code).is_ok() {
+        autocfg::emit(cfg);
+    }
+}
+
+fn assert_matches_in_module(ac: &AutoCfg) {
+    let cfg = "assert_matches_in_module";
+    let code = r#"
+    #![allow(stable_features)]
+    #![feature(assert_matches)]
+    use std::assert_matches::assert_matches;
+
+    fn main() {
+        assert_matches!(Some(4), Some(_));
+    }
+        "#;
+    autocfg::emit_possibility(cfg);
+    if ac.probe_raw(code).is_ok() {
+        autocfg::emit(cfg);
+    }
 }
