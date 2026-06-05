f210a9d9738148e2394012de3dec98ed48807f61
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-05 13:47:11 +0000

    use git for ninja-build

--- build.rs
+++ build.rs
@@ -1,4 +1,3 @@
-use autocfg::AutoCfg;
 use ninja_build_rs::{
     Result,
     nightly::{AssertMatchesLocation, Nightly},
@@ -13,27 +12,7 @@ fn main() -> Result<()> {
     }
     ac.emit_unstable_feature("never_type");
     ac.emit_unstable_feature("proc_macro_diagnostic");
-    emit_unstable_pmdiag(&ac);
     ac.emit_unstable_feature("try_trait_v2");
     ac.emit_unstable_feature("try_trait_v2_residual");
     Ok(())
-}
-
-fn emit_unstable_pmdiag(ac: &AutoCfg) {
-    let feature = "proc_macro_diagnostic";
-    let cfg = format!("unstable_{feature}");
-    // #![allow(unused)] is required to avoid this failing for `cargo clippy -- -D warnings`
-    let code = format!(
-        r#"
-        #![deny(stable_features)]
-        #![allow(unused)]
-        #![feature({feature})]
-        extern crate proc_macro;
-        "#
-    );
-    dbg!(&code);
-    autocfg::emit_possibility(&cfg);
-    if ac.probe_raw(&code).is_ok() {
-        autocfg::emit(&cfg);
-    }
 }
