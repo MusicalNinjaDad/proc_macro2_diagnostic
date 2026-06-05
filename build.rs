f6a992460cbc24d48fb7a6ef0778b771c71df6cc
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-05 14:17:00 +0000

    use new assert_matches location output

--- build.rs
+++ build.rs
@@ -1,15 +1,8 @@
-use ninja_build_rs::{
-    Result,
-    nightly::{AssertMatchesLocation, Nightly},
-};
+use ninja_build_rs::{Result, nightly::Nightly};
 
 fn main() -> Result<()> {
     let ac = autocfg::new();
     ac.emit_unstable_feature("assert_matches");
-    AssertMatchesLocation::emit_possibilities();
-    if let Some(location) = ac.assert_matches_location() {
-        autocfg::emit(&location.to_string())
-    }
     ac.emit_unstable_feature("never_type");
     ac.emit_unstable_feature("proc_macro_diagnostic");
     ac.emit_unstable_feature("try_trait_v2");
