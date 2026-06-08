8725c8ccc00977a25df32a988c37d161e65c538e
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-08 15:02:30 +0000

    add test for no_diagnostic

--- tests/test_compilation.rs
+++ tests/test_compilation.rs
@@ -3,11 +3,17 @@ use trybuild::{self, TestCases};
 #[test]
 fn failures() {
     let t = TestCases::new();
+    #[cfg(has_proc_macro_diagnostic)]
     t.compile_fail("tests/compilation/examples/fail_*.rs");
+    #[cfg(not(has_proc_macro_diagnostic))]
+    t.compile_fail("tests/compilation/examples/no_diagnostic/fail_*.rs");
 }
 
 #[test]
 fn ok() {
     let t = TestCases::new();
+    #[cfg(has_proc_macro_diagnostic)]
     t.pass("tests/compilation/examples/pass_*.rs");
+    #[cfg(not(has_proc_macro_diagnostic))]
+    t.pass("tests/compilation/examples/no_diagnostic/pass_*.rs");
 }
