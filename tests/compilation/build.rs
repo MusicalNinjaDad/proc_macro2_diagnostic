0c5c18da1ae8a9c8fad3e7b2a895dfc90ce64e97
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-08 14:59:27 +0000

    parse env var in fixture

--- tests/compilation/build.rs
+++ tests/compilation/build.rs
@@ -1,11 +1,23 @@
-use ninja_build_rs::{Result, nightly::Nightly};
+use ninja_build_rs::{Result, nightly::Nightly, split_var};
 
 fn main() -> Result<()> {
     let ac = autocfg::new();
     ac.emit_unstable_feature("assert_matches");
-    ac.emit_unstable_feature("never_type");
-    ac.emit_unstable_feature("proc_macro_diagnostic");
-    ac.emit_unstable_feature("try_trait_v2");
-    ac.emit_unstable_feature("try_trait_v2_residual");
+    let test_flags = split_var("PROC_MACRO2_DIAGNOSTIC_TEST").unwrap_or_default();
+    if test_flags.contains("no_diagnostic") {
+        autocfg::emit_possibility("unstable_proc_macro_diagnostic");
+        autocfg::emit_possibility("has_proc_macro_diagnostic");
+    } else {
+        ac.emit_unstable_feature("proc_macro_diagnostic");
+    }
+    if test_flags.contains("no_try") {
+        autocfg::emit_possibility("never_type");
+        autocfg::emit_possibility("try_trait_v2");
+        autocfg::emit_possibility("try_trait_v2_residual");
+    } else {
+        ac.emit_unstable_feature("never_type");
+        ac.emit_unstable_feature("try_trait_v2");
+        ac.emit_unstable_feature("try_trait_v2_residual");
+    }
     Ok(())
 }
