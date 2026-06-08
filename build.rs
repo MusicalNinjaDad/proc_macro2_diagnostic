5fc207c93aa211b055de0797b7bce7e3b8aa3299
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-08 07:32:51 +0000

    parse multiple values from env var

--- build.rs
+++ build.rs
@@ -1,10 +1,19 @@
+use std::collections::HashSet;
+
 use ninja_build_rs::{Result, get_var, nightly::Nightly};
 
 fn main() -> Result<()> {
     let ac = autocfg::new();
     ac.emit_unstable_feature("assert_matches");
+    let test_flags: HashSet<String> = if let Ok(flags) = get_var("PROC_MACRO2_DIAGNOSTIC_TEST") {
+        std::env::split_paths(&flags)
+            .map(|p| p.to_string_lossy().to_string())
+            .collect()
+    } else {
+        Default::default()
+    };
     ac.emit_unstable_feature("never_type");
-    if get_var("PROC_MACRO2_DIAGNOSTIC_TEST").is_ok() {
+    if test_flags.contains("no_diagnostic") {
         autocfg::emit_possibility("unstable_proc_macro_diagnostic");
         autocfg::emit_possibility("has_proc_macro_diagnostic");
     } else {
