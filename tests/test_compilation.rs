0e8a102ad5ad302dda4ccef2018e1a27de2f29fd
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-08 18:13:23 +0000

    simplify test path generation

--- tests/test_compilation.rs
+++ tests/test_compilation.rs
@@ -1,19 +1,28 @@
+use std::path::PathBuf;
+
 use trybuild::{self, TestCases};
+
+#[expect(clippy::no_effect)]
+fn examples(path: &str) -> PathBuf {
+    let mut examples = PathBuf::from("tests/compilation/examples");
+    cfg_select! {
+        not(has_proc_macro_diagnostic) => examples.push("no_diagnostic"),
+        not(has_try_trait_v2) => examples.push("no_try"),
+        _ => (),
+    };
+    examples.push(path);
+    examples
+}
 
 #[test]
 fn failures() {
     let t = TestCases::new();
-    #[cfg(has_proc_macro_diagnostic)]
-    t.compile_fail("tests/compilation/examples/fail_*.rs");
-    #[cfg(not(has_proc_macro_diagnostic))]
-    t.compile_fail("tests/compilation/examples/no_diagnostic/fail_*.rs");
+    t.compile_fail(examples("fail_*.rs"));
 }
 
 #[test]
 fn ok() {
     let t = TestCases::new();
     #[cfg(has_proc_macro_diagnostic)]
-    t.pass("tests/compilation/examples/pass_*.rs");
-    #[cfg(not(has_proc_macro_diagnostic))]
-    t.pass("tests/compilation/examples/no_diagnostic/pass_*.rs");
+    t.pass(examples("pass_*.rs"));
 }
