bb3fb2358437754294587ac4183b35a39323647c
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-09 12:12:14 +0000

    move to build_safely 0.5.0

--- build.rs
+++ build.rs
@@ -1,21 +1,17 @@
-use ninja_build_rs::{
-    Result,
-    nightly::{Nightly, cargo_allowed_features},
-    split_var,
-};
+use build_safely::prelude::*;
 
 fn main() -> Result<()> {
     let mut ac = autocfg::new();
     ac.set_edition(Some("2024".to_string()));
 
     let allowed_features = cargo_allowed_features()?;
-    ac.emit_unstable_feature("assert_matches", &allowed_features);
+    ac.emit_unstable_feature(assert_matches, &allowed_features);
     let test_flags = split_var("PROC_MACRO2_DIAGNOSTIC_TEST").unwrap_or_default();
     if test_flags.contains("no_diagnostic") {
         autocfg::emit_possibility("unstable_proc_macro_diagnostic");
         autocfg::emit_possibility("has_proc_macro_diagnostic");
     } else {
-        ac.emit_unstable_feature("proc_macro_diagnostic", &allowed_features);
+        ac.emit_unstable_feature(proc_macro_diagnostic, &allowed_features);
     }
     if test_flags.contains("no_try") {
         autocfg::emit_possibility("unstable_iterator_try_collect");
@@ -27,10 +23,10 @@ fn main() -> Result<()> {
         autocfg::emit_possibility("unstable_try_trait_v2_residual");
         autocfg::emit_possibility("has_try_trait_v2_residual");
     } else {
-        ac.emit_unstable_feature("iterator_try_collect", &allowed_features);
-        ac.emit_unstable_feature("never_type", &allowed_features);
-        ac.emit_unstable_feature("try_trait_v2", &allowed_features);
-        ac.emit_unstable_feature("try_trait_v2_residual", &allowed_features);
+        ac.emit_unstable_feature(iterator_try_collect, &allowed_features);
+        ac.emit_unstable_feature(never_type, &allowed_features);
+        ac.emit_unstable_feature(try_trait_v2, &allowed_features);
+        ac.emit_unstable_feature(try_trait_v2_residual, &allowed_features);
     }
     Ok(())
 }
--- tests/compilation/build.rs
+++ tests/compilation/build.rs
@@ -1,20 +1,16 @@
-use ninja_build_rs::{
-    Result,
-    nightly::{Nightly, cargo_allowed_features},
-    split_var,
-};
+use build_safely::prelude::*;
 
 fn main() -> Result<()> {
     let ac = autocfg::new();
     let allowed_features = cargo_allowed_features()?;
 
-    ac.emit_unstable_feature("assert_matches", &allowed_features);
+    ac.emit_unstable_feature(assert_matches, &allowed_features);
     let test_flags = split_var("PROC_MACRO2_DIAGNOSTIC_TEST").unwrap_or_default();
     if test_flags.contains("no_diagnostic") {
         autocfg::emit_possibility("unstable_proc_macro_diagnostic");
         autocfg::emit_possibility("has_proc_macro_diagnostic");
     } else {
-        ac.emit_unstable_feature("proc_macro_diagnostic", &allowed_features);
+        ac.emit_unstable_feature(proc_macro_diagnostic, &allowed_features);
     }
     if test_flags.contains("no_try") {
         autocfg::emit_possibility("unstable_never_type");
@@ -24,9 +20,9 @@ fn main() -> Result<()> {
         autocfg::emit_possibility("unstable_try_trait_v2_residual");
         autocfg::emit_possibility("has_try_trait_v2_residual");
     } else {
-        ac.emit_unstable_feature("never_type", &allowed_features);
-        ac.emit_unstable_feature("try_trait_v2", &allowed_features);
-        ac.emit_unstable_feature("try_trait_v2_residual", &allowed_features);
+        ac.emit_unstable_feature(never_type, &allowed_features);
+        ac.emit_unstable_feature(try_trait_v2, &allowed_features);
+        ac.emit_unstable_feature(try_trait_v2_residual, &allowed_features);
     }
     Ok(())
 }
