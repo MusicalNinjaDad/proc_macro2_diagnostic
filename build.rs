24c80c7e021d7d72ea7f748492111aa139fd3320
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-09 14:02:19 +0000

    remove test env var handling from build scripts

--- build.rs
+++ build.rs
@@ -6,27 +6,13 @@ fn main() -> Result<()> {
 
     let allowed_features = cargo_allowed_features()?;
     ac.emit_unstable_feature(assert_matches, &allowed_features);
-    let test_flags = split_var("PROC_MACRO2_DIAGNOSTIC_TEST").unwrap_or_default();
-    if test_flags.contains("no_diagnostic") {
-        autocfg::emit_possibility("unstable_proc_macro_diagnostic");
-        autocfg::emit_possibility("has_proc_macro_diagnostic");
-    } else {
-        ac.emit_unstable_feature(proc_macro_diagnostic, &allowed_features);
-    }
-    if test_flags.contains("no_try") {
-        autocfg::emit_possibility("unstable_iterator_try_collect");
-        autocfg::emit_possibility("has_iterator_try_collect");
-        autocfg::emit_possibility("unstable_never_type");
-        autocfg::emit_possibility("has_never_type");
-        autocfg::emit_possibility("unstable_try_trait_v2");
-        autocfg::emit_possibility("has_try_trait_v2");
-        autocfg::emit_possibility("unstable_try_trait_v2_residual");
-        autocfg::emit_possibility("has_try_trait_v2_residual");
-    } else {
-        ac.emit_unstable_feature(iterator_try_collect, &allowed_features);
-        ac.emit_unstable_feature(never_type, &allowed_features);
-        ac.emit_unstable_feature(try_trait_v2, &allowed_features);
-        ac.emit_unstable_feature(try_trait_v2_residual, &allowed_features);
-    }
+
+    ac.emit_unstable_feature(proc_macro_diagnostic, &allowed_features);
+
+    ac.emit_unstable_feature(iterator_try_collect, &allowed_features);
+    ac.emit_unstable_feature(never_type, &allowed_features);
+    ac.emit_unstable_feature(try_trait_v2, &allowed_features);
+    ac.emit_unstable_feature(try_trait_v2_residual, &allowed_features);
+
     Ok(())
 }
